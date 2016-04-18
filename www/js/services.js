ad40166b0f9b57b8d6aa56b07abc598a5a7327f5
peidong yuan <yuanpeidong1991@gmail.com>
2016-04-18 16:43:37 +0800

    user login/signup features achieved

--- www/js/services.js
+++ www/js/services.js
@@ -15,3 +15,24 @@ angular.module('starter.services', [])
     }
   }
 })
+.factory('loginService',function($http){
+  return{
+    CreateNewUser : function(user){
+      console.log(user);
+      $http.post('http://localhost:3030/signup/', user);
+    },
+    onLogin : function(email,password){
+      console.log(email,password);
+      app.authenticate({
+        type: 'local',
+        'email': email,
+        'password': password
+      }).then(function(result){
+        console.log('Authenticated!', app.get('token'));
+      }).catch(function(error){
+        console.error('Error authenticating!', error);
+      });
+    }
+  }
+
+})
